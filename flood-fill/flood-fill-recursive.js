78d84ce5130e20c9c38d1e9057c526766ef22190
Matt Jennings <matt@mattjennings.net>
2024-10-10 12:42:23 -0700

    added basic prettier

--- flood-fill/flood-fill-recursive.js
+++ flood-fill/flood-fill-recursive.js
@@ -7,38 +7,38 @@ Return a boolean that is true if and only if the matrix was modified.
 */
 
 const directions = [
-  [1, 0], // down
-  [-1, 0], // up
-  [0, 1], // right
-  [0, -1] // left
+	[1, 0], // down
+	[-1, 0], // up
+	[0, 1], // right
+	[0, -1], // left
 ];
 
 function floodFill(matrix, r, c, val) {
-  const oldVal = matrix[r][c];
-  const modified = oldVal !== val;
+	const oldVal = matrix[r][c];
+	const modified = oldVal !== val;
 
-  if (modified) {
-    // we only need to do work if the color is not yet set
-    // this prevents infinite loops
-    matrix[r][c] = val;
+	if (modified) {
+		// we only need to do work if the color is not yet set
+		// this prevents infinite loops
+		matrix[r][c] = val;
 
-    for (const [dR, dC] of directions) {
-      // this might be out of bounds
-      // use optional chaining short
-      if (matrix[r + dR]?.[c + dC] === oldVal) {
-        floodFill(matrix, r + dR, c + dC, val);
-      }
-    }
-  }
+		for (const [dR, dC] of directions) {
+			// this might be out of bounds
+			// use optional chaining short
+			if (matrix[r + dR]?.[c + dC] === oldVal) {
+				floodFill(matrix, r + dR, c + dC, val);
+			}
+		}
+	}
 
-  // return matrix;
-  return modified;
+	// return matrix;
+	return modified;
 }
 
 const matrix = [
-  [1, 1, 1],
-  [1, 1, 1],
-  [1, 2, 2]
+	[1, 1, 1],
+	[1, 1, 1],
+	[1, 2, 2],
 ];
 
 console.log(floodFill(matrix, 1, 1, 3));
--- move-zeros-to-end-of-array/move-zeros-to-end-of-array.js
+++ move-zeros-to-end-of-array/move-zeros-to-end-of-array.js
@@ -12,33 +12,33 @@ For example, given the input array `[0, 1, 0, 3, 12]`, after calling your functi
 */
 
 function moveZerosToEnd(arr) {
-    let length = arr.length;
+	let length = arr.length;
 
-    // If the length of the array is less than 2, there's no need to perform any operations,
-    // so we return the array as is
-    if(length < 2) return arr;
+	// If the length of the array is less than 2, there's no need to perform any operations,
+	// so we return the array as is
+	if (length < 2) return arr;
 
-    // Initialize a variable 'counter' to keep track of the current position where non-zero elements should be placed
-    let counter = 0;
+	// Initialize a variable 'counter' to keep track of the current position where non-zero elements should be placed
+	let counter = 0;
 
-    // Iterate through each element of the array
-    for(let i = 0; i < length; i++) {
-        // If the element is non-zero, move it to the current position indicated by 'counter' and increment 'counter'
-        if(arr[i] !== 0) {
-            arr[counter] = arr[i];
-            counter += 1;
-        }
-    }
+	// Iterate through each element of the array
+	for (let i = 0; i < length; i++) {
+		// If the element is non-zero, move it to the current position indicated by 'counter' and increment 'counter'
+		if (arr[i] !== 0) {
+			arr[counter] = arr[i];
+			counter += 1;
+		}
+	}
 
-    // Fill the remaining positions in the array with zeros
-    // by continuing to increment 'counter' until it reaches the length of the array
-    while(counter < length) {
-        arr[counter] = 0;
-        counter += 1;
-    }
+	// Fill the remaining positions in the array with zeros
+	// by continuing to increment 'counter' until it reaches the length of the array
+	while (counter < length) {
+		arr[counter] = 0;
+		counter += 1;
+	}
 
-    // Return the modified array, where all non-zero elements have been moved to the end followed by zeros
-    return arr;
+	// Return the modified array, where all non-zero elements have been moved to the end followed by zeros
+	return arr;
 }
 
 console.log(moveZerosToEnd([0, 1, 0, 3, 12]));
